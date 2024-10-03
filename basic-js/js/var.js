71cf9ced12cc8e1c156a3c09d9cc7c202f5b0466
Greed_41 <103091733+dirga41@users.noreply.github.com>
2024-10-03 23:44:31 +0700

    Add files via upload

---
+++ basic-js/js/var.js
@@ -0,0 +1,32 @@
+console.log("------ Variables ------");
+// Var
+for (var i = 0; i < 5; i++) {
+    console.log(i);
+}
+console.log(i);
+// Var
+function looping() {
+    for ( j = 0; j < 5; j++) {
+        console.log(j);
+    }
+}
+looping();
+console.log(j);
+// Let
+for ( k = 0; k < 5; k++) {
+    console.log(k);
+}
+
+console.log(k);
+// Let
+for (let l = 0; l < 5; l++) {
+    console.log(l);
+}
+if (true) {
+    let l = 100;
+    console.log("l =", l);
+}
+// Const
+const phi = 3.14;
+ // phi = 3.147; wrong
+console.log(phi);
